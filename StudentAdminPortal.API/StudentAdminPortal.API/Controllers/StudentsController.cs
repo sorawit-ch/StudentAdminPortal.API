34e9b8af96da4540cf271f12de697047f04e6763
sorawit2560 <sorawit72@hotmail.com>
2022-08-26 00:51:05 +0700

    delete student

--- StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentsController.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentsController.cs
@@ -63,5 +63,18 @@ namespace StudentAdminPortal.API.Controllers
         }
 
 
+        [HttpDelete]
+        [Route("[controller]/{studentId:guid}")]
+        public async Task<IActionResult> DeleteStudentAsync([FromRoute] Guid studentId)
+        {
+            if(await studentRepository.Exists(studentId))
+            {
+                var student = await studentRepository.DeleteStudent(studentId);
+                return Ok(mapper.Map<Student>(student));
+            }
+            return NotFound();
+        }
+
+
     }
 }
